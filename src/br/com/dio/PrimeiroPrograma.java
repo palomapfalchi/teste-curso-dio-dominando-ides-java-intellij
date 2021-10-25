49e8fd4d2e0203daf7743b3447856df7433c0fea
Paloma Falchi <palomapfalchi@gmail.com>
2021-10-25 18:04:52 -0300

    Add Livro1

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,56 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        /*int a = 5;
-        int b = 3;
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+              /*int a = 5;        int b = 3;
 
         System.out.println("Hello word" + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer nuPaginas;
+
+    public Livro(String nome, Integer nuPaginas) {
+        this.nome = nome;
+        this.nuPaginas = nuPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNuPaginas() {
+        return nuPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nuPaginas=" + nuPaginas +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
